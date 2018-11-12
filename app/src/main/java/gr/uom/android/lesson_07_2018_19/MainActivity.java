df946c7d7379854020d757dc2d58f0430762fc5d
teo <teohaik@gmail.com>
2018-11-12 20:46:13 +0200

    initial2

---
+++ app/src/main/java/gr/uom/android/lesson_07_2018_19/MainActivity.java
@@ -0,0 +1,16 @@
+package gr.uom.android.lesson_07_2018_19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
